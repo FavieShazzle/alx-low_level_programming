aa40b226aef99f2445af856209fe109235fdbb3b
FavieShazzle <faviekay6@gmail.com>
2022-06-28 15:38:51 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+* main - the entry point
+* Description: This functions print...
+* 
+* Return: 0
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
