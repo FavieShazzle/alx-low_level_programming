1e174141971ac288da31b31915eb8ea73f59f414
FavieShazzle <faviekay6@gmail.com>
2022-06-28 16:14:28 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Description: This function prints a test
+ * Return: 0 to indicate success
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0)
+}
